e63e5ecb450e1a09648592485bbbbae002f358ea
Calvin Low <lowcheeseng0309@gmail.com>
2024-08-11 20:11:25 +0800

    add header

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,6 +123,7 @@ default_countries = [
     'Korea, Rep.', 'Malaysia', 'Thailand'
 ]
 st.set_page_config(page_title="TGP Dashboard", layout="wide")
+st.title("Digital Reach & Financial Access Dashboard for PayNet")
 
 # Create a region filter
 regions = ['All'] + sorted(dim_country_df['region'].dropna().unique())
